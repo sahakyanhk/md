a03984aa06d0f1b3141581939e30ff42d8bf69e9
Harutyun Sahakyan <69894030+sahakyanhk@users.noreply.github.com>
2021-10-06 09:46:07 +0400

    Update GMX_install.sh

--- GMX_install.sh
+++ GMX_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 # !!! This script was tested for nvidia/cuda:10.2-devel-ubuntu18.04 image 
 
 echo y | apt-get install wget emacs cmake
 
+#download gromacs
 wget  ftp://ftp.gromacs.org/pub/gromacs/gromacs-2020.4.tar.gz
 
 tar xfz gromacs-2020.4.tar.gz
@@ -11,6 +11,7 @@ cd gromacs-2020.4
 mkdir build 
 cd build 
 
+#installation, see more at https://manual.gromacs.org/documentation/current/install-guide/index.html
 cmake .. -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=on  #-DGMX_SIMD=SSE4.1
 make -j 8
 make install
@@ -19,5 +20,4 @@ echo source /usr/local/gromacs/bin/GMXRC >> ~/.bashrc
 
 cd 
 rm -r grom*
-
 bash
